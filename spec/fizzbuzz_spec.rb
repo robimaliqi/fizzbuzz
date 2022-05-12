1ea605538742166200b69d0eafe442f3682b87e2
benjamin <b.james.neustadt@gmail.com>
2022-05-12 13:43:34 +0200

    updated lib file for number=number

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,13 +5,12 @@ describe 'fizzbuzz' do
     expect(3.fizzbuzz).to eq 'fizz'
   end
   it 'returns "buzz" when passed 5' do
-<<<<<<< HEAD
     expect(5.fizzbuzz).to eq 'fizz'
   end
   it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
     expect(15.fizzbuzz).to eq 'fizzbuzz'
-=======
-    expect(fizzbuzz(5)).to eq 'buzz'
   end
->>>>>>> ab92a5e65c506de74ea667be52d92ef068703b41
+  it 'returns "number" when not passed multiple of 3 OR 5' do
+    expect(7.fizzbuzz). to eq '7'
+  end
 end
