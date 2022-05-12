7fb24313911df0bbd7e66bc02bcb2c31ecfb3822
Robi <robi.maliqi@hotmail.co.uk>
2022-05-12 12:29:19 +0100

    third test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,11 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(3.fizzbuzz).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(5.fizzbuzz).to eq 'fizz'
+  end
+  it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
 end
